90dda7d3424802fc4ce8a93e1cae44389134bf94
Renelle Alemios <villamoralemios123@gmail.com>
2026-05-05 15:54:30 +0800

    fix all the erorrs and lackings

---
+++ src/polyfills.ts
@@ -0,0 +1,32 @@
+/*
+ * This file includes polyfills needed by Angular and is loaded before the app.
+ * You can add your own extra polyfills to this file.
+ *
+ * This file is divided into 2 sections:
+ * 1. Browser polyfills (loaded before ZoneJS)
+ * 2. Application imports (loaded after ZoneJS)
+ *
+ * The current setup is for "evergreen" browsers (latest versions of Chrome, Edge, Firefox, Safari, etc.)
+ *
+ * Learn more: https://angular.io/guide/browser-support
+ */
+
+/***************************************************************************************************
+ * BROWSER POLYFILLS
+ */
+
+/**
+ * By default, zone.js patches all async APIs (setTimeout, DOM events, etc.)
+ * You can disable some patches using zone-flags.ts if needed.
+ */
+
+// import './zone-flags';
+
+/***************************************************************************************************
+ * Zone JS is required by Angular
+ */
+import 'zone.js';  // Included with Angular CLI
+
+/***************************************************************************************************
+ * APPLICATION IMPORTS
+ */
